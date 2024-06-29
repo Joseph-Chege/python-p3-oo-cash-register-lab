3ab8bb5df5d3ac39af19beee8d09c060363be9da
Joseph-Chege <mjchege23@gmail.com>
2024-06-29 11:50:01 +0300

    Created the Cash Register App

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,41 @@
 #!/usr/bin/env python3
 
 class CashRegister:
+  def __init__(self, discount=0):
+    #Setting instance variables to respective defaults
+    self.discount = discount
+    self.total = 0
+    self.items = []
+    self.last_transaction = 0
+
+  def add_item(self, title, price, quantity=1):
+    #Adding the item to the list of items
+    for _ in range(quantity):
+      self.items.append(title)
+    #Adding the price of the item to the total
+    self.total += price * quantity
+    self.last_transaction = price * quantity
+
+  def apply_discount(self):
+    #Checking if there is a discount
+    if self.discount == 0:
+      print("There is no discount to apply.")
+    else:
+      #Applying the discount to the total
+      self.total -= int(self.total * self.discount / 100)
+      print(f"After the discount, the total comes to ${self.total}.")
+      
+  def items(self):
+    #Returning the list of items
+    return self.items
+  
+  def void_last_transaction(self):
+    #Subtracting the last item from the total
+    self.total -= self.last_transaction
+    self.items = self.items[:-1]
+
+  
+  
+
+
   pass
